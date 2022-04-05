926d4ae1ded1db7934bb231e4a94a8f2194d2786
emilymorgenbesser <emorgenbesser@sigmasolve.net>
2022-04-04 23:01:42 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace c__.net5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myName = "Emily"; 
+            Console.WriteLine("Hello "+ myName);
+        }
+    }
+}
